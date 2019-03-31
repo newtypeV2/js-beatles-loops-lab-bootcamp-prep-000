993d7695ccb55d9279d5606d66a4282a66b4ffdb
Gian Libunao <gianlibunao@gmail.com>
2019-03-31 22:34:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+var array = []
+
+function theBeatlesPlay(musicians,instrument){
+for(var i=0;i<musicians.length;i++){
+  if(i===0){
+  musicians[i]=
+  instrument[i]=
+  }
+}
+}

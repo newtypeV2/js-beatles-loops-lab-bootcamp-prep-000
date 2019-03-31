8ea1befe2243c5c227092a69334c486cca37eb3a
Gian Libunao <gianlibunao@gmail.com>
2019-03-31 22:35:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ var array = []
 function theBeatlesPlay(musicians,instrument){
 for(var i=0;i<musicians.length;i++){
   if(i===0){
-  musicians[i]=
-  instrument[i]=
+  musicians[i]="John Lennon plays guitar"
+  instrument[i]="John Lennon plays guitar"
+  array.push("John Lennon plays guitar")
+  }
+  else{
+    
   }
 }
 }

c210f5e0f02902fdf25fbc90e7c8420650e595d8
Gian Libunao <gianlibunao@gmail.com>
2019-03-31 22:40:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ for(var i=0;i<musicians.length;i++){
 const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
-function
+theBeatlesPlay(musicians,instruments)
 
 for(var i=0;i<musicians.length;i++){
   console.log(array[i])
